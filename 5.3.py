ea77f90c7c08c57588e6d38f4a4b30185bb5ead9
alisherweb3 <42702073+alisherweb3@users.noreply.github.com>
2022-09-21 22:17:28 +0500

    Create 5.3.py

---
+++ 5.3.py
@@ -0,0 +1,12 @@
+tf.image.per_image_whitening(image)
+
+# Su'niy ravishda Rasmlarni 
+
+tf.random_crop(value, size, seed=None, name=None)
+tf.image.random_flip_up_down(image, seed=None)
+tf.image.random_flip_left_right(image, seed=None)
+tf.image.transpose_image(image)
+tf.image.random_brightness(image, max_delta, seed=None)
+tf.image.random_contrast(image, lower, upper, seed=None)
+tf.image.random_saturation(image, lower, upper, seed=None)
+tf.image.random_hue(image, max_delta, seed=None)
